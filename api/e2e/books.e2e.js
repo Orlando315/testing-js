169e01d83c328c4faf1de620c58ae8b49ad6d25f
Orlando Perez <Orlandoj_315@hotmail.com>
2022-08-21 13:47:41 -0400

    Integration test

---
+++ api/e2e/books.e2e.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+
+const mockSpyGetAll = jest.fn();
+
+const createApp = require('../src/app');
+const { generateManyBooks } = require('../src/fakes/book.fake');
+
+jest.mock('../src/lib/mongo.lib.js', () => jest.fn().mockImplementation(() => ({
+  getAll: mockSpyGetAll,
+  create: () => {},
+})));
+
+describe('Test for books', () => {
+  let app = null;
+  let server = null;
+
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('Test for [GET] /api/v1/books', () => {
+    test('should return a list of books', () => {
+      // Arrange
+      const fakeBooks = generateManyBooks(3);
+      mockSpyGetAll.mockResolvedValue(fakeBooks);
+      // Act
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          // Assert
+          expect(body.length).toEqual(fakeBooks.length);
+        });
+    });
+  });
+});
